10c24cbd5b9e2d8dbd36471d58b475e431130fe8
PolinaTyaglitsova <polina.vladimirovna.t@gmail.com>
2024-11-17 18:45:02 +0300

    добавлена задача 1 лабы 2

---
+++ laba2.1.py
@@ -0,0 +1,11 @@
+x = int(input())
+
+def function(x):
+    if (x >= -2) and (x < 2):
+        return x * x
+    elif (x >= 2):
+        return x * x + 4 * x + 5
+    elif (x < -2):
+        return 4
+
+print(function(x))
