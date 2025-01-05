f7d857a2497d9a9c31d5de4960ad1d153ade08e0
PolinaTyaglitsova <polina.vladimirovna.t@gmail.com>
2025-01-05 21:54:17 +0300

    добавлена задача 2 лабы 5

---
+++ laba5.3.py
@@ -0,0 +1,10 @@
+#Дана строка, содержащая предложение
+# Напишите программу, которая:
+#разделит предложение на отдельные слова
+#подсчитает, сколько слов в предложении
+sentence = "Python is a powerful and easy-to-learn programmig language"
+word = sentence.replace('-', ' ').split(' ')
+#word = sentence.split(' ')
+#print(word)
+print('\n'.join(word))
+print(len(word))
